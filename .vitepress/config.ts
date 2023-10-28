7507d9089337c3436106f32ddae720bcd06c1c4e
alkanoic <alkanoic@gmail.com>
2023-10-28 19:23:14 +0900

    VitePressのアップデート

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,32 +1,30 @@
-import { SearchPlugin } from "vitepress-plugin-search";
-import { createMarkdownRenderer, defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
+import markdownItPlantuml from 'markdown-it-plantuml';
 
 var options = {
   previewLength: 62,
-  buttonLabel: "Search",
-  placeholder: "Search docs",
+  buttonLabel: 'Search',
+  placeholder: 'Search docs',
 };
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "VitePress Sample",
-  description: "VitePress Sample Description",
+  title: 'VitePress Sample',
+  description: 'VitePress Sample Description',
   cleanUrls: true,
-  lang: "ja-JP",
+  lang: 'ja-JP',
   lastUpdated: true,
   markdown: {
     lineNumbers: true,
-    config: (md) =>{
-      md.use(require('markdown-it-plantuml'), {
-        server: "http://localhost:8080",
-      })
-    }
+    config: (md) => {
+      md.use(markdownItPlantuml);
+    },
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -35,13 +33,11 @@ export default defineConfig({
         collapsed: true,
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/alkanoic/VitePressSample' }
-    ]
+    socialLinks: [{ icon: 'github', link: 'https://github.com/alkanoic/VitePressSample' }],
   },
-})
+});
